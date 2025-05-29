426c685a6bacd07fab2c26b961195231698e766c
서유진 <seoyujin@seoyujin-ui-MacBookAir.local>
2025-05-29 22:57:31 +0900

    Update result template and app logic

--- summer_test/app.py
+++ summer_test/app.py
@@ -15,13 +15,14 @@ def result():
             score += int(answer)
 
     if score >= 4:
-        result_text = "🌲 당신은 싱그러운 산 속 '강원 영월형 인간'이군요!"
+        result_text = "🌲 당신은 싱그러운 산 속<br>'강원 영월형 인간'이군요!"
         image = "영월.jpg"
     else:
-        result_text = "🌾 당신은 따스한 평야의 '충남 당진형 인간'이군요!"
+        result_text = "🌾 당신은 따스한 평야의<br>'충남 당진형 인간'이군요!"
         image = "당진.jpg"
 
     return render_template("result.html", result=result_text, image=image)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
+
